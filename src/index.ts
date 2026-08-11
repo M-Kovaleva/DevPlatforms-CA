8953fdf8e4d04d2c11aa36488684c23c90850a1f
M-Kovaleva <owlet.savvina@gmail.com>
2026-08-11 14:58:25 +0200

    Add .env and CORS

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "News API works!" });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server works on ${PORT} port `);
+  console.log(`Server works on ${PORT} port`);
 });
