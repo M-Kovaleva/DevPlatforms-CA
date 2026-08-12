cabc2bf7fe23ded9771fe20e65b35d302e96392b
M-Kovaleva <owlet.savvina@gmail.com>
2026-08-12 14:33:56 +0200

    Adding database.ts, connecting to MySQL

---
+++ src/database.ts
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import mysql, { Pool } from "mysql2/promise";
+
+// Create a connection pool to manage database connections
+const pool: Pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+export { pool };
